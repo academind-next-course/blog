54cb3056b21976bcf64cdcadce672211c4ee06fe
Guido Perdomo <guidop91@gmail.com>
2026-07-24 14:29:19 -0400

    Add news page routing, remove DS_Store, add to gitignore

---
+++ app/news/page.js
@@ -0,0 +1,12 @@
+export default function News() {
+  return (
+    <main>
+      <h1>News</h1>
+      <ul>
+        <li><a href="/news/first-news-site">First news site</a></li>
+        <li><a href="/news/second-news-site">Second news site</a></li>
+        <li><a href="/news/third-news-site">Third news site</a></li>
+      </ul>
+    </main>
+  )
+}
